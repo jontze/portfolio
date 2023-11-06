aa8ad526d7a5943220bd849bcf244c1f65286333
Jontze <42588836+jontze@users.noreply.github.com>
2023-11-07 00:20:28 +0100

    feat(card): Create cart and card-title component

---
+++ libs/ui/card/src/lib/card-title/card-title.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'portfolio-card-title',
+  templateUrl: './card-title.component.html',
+})
+export class CardTitleComponent {}
--- libs/ui/card/src/lib/card.module.ts
+++ libs/ui/card/src/lib/card.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CardComponent } from './card/card.component';
+import { CardTitleComponent } from './card-title/card-title.component';
 
 @NgModule({
   imports: [CommonModule],
+  declarations: [CardComponent, CardTitleComponent],
+  exports: [CardComponent, CardTitleComponent],
 })
 export class CardModule {}
