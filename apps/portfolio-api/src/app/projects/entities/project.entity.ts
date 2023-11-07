d02dd2318531f048f113f0128ac4a1dbb0eca6ed
Jontze <42588836+jontze@users.noreply.github.com>
2023-11-08 00:45:08 +0100

    feat(api): Fetch and provide pinned repos and cache them for 5 mins

--- apps/portfolio-api/src/app/projects/entities/project.entity.ts
+++ apps/portfolio-api/src/app/projects/entities/project.entity.ts
@@ -1,1 +1,13 @@
-export class Project {}
+export class Project {
+  name: string;
+  description: string;
+  url: string;
+  stargazerCount: number;
+  forkCount: number;
+  createdAt: string;
+  updatedAt: string;
+  primaryLanguage?: {
+    name: string;
+    color?: string;
+  };
+}
--- apps/portfolio-api/src/app/projects/projects.controller.ts
+++ apps/portfolio-api/src/app/projects/projects.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, UseInterceptors } from '@nestjs/common';
+import { CacheInterceptor } from '@nestjs/cache-manager';
 import { ProjectsService } from './projects.service';
 
 @Controller('projects')
+@UseInterceptors(CacheInterceptor)
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
   @Get()
   findAll() {
-    return this.projectsService.findAll();
+    const token = process.env.GITHUB_TOKEN;
+    if (!token) {
+      throw new Error('Missing GITHUB_TOKEN env variable');
+    }
+    return this.projectsService.findAll('jontze', token);
   }
 
   @Get(':id')
