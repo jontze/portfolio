89562f6e65fee9d3afec66bca365c7e7b78b81ed
jontze <42588836+jontze@users.noreply.github.com>
2023-11-11 14:43:12 +0100

    feat(card): Add simple card spotlight

--- libs/ui/card/src/lib/card.module.ts
+++ libs/ui/card/src/lib/card.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './card/card.component';
 import { CardTitleComponent } from './card-title/card-title.component';
+import { DirectiveModule } from '@jontze/util/directive';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, DirectiveModule],
   declarations: [CardComponent, CardTitleComponent],
   exports: [CardComponent, CardTitleComponent],
 })
--- libs/ui/card/src/lib/card/card.component.ts
+++ libs/ui/card/src/lib/card/card.component.ts
@@ -1,8 +1,27 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  WritableSignal,
+  computed,
+  signal,
+} from '@angular/core';
 
 @Component({
   selector: 'portfolio-card',
   templateUrl: './card.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CardComponent {}
+export class CardComponent {
+  mouseX: WritableSignal<number> = signal(0);
+  mouseY: WritableSignal<number> = signal(0);
+
+  mouseStyle = computed(() => ({
+    '--mouse-x': `${this.mouseX()}px`,
+    '--mouse-y': `${this.mouseY()}px`,
+  }));
+
+  onMouseMoveOnCard(event: { xPixel: number; yPixel: number }): void {
+    this.mouseX.set(event.xPixel);
+    this.mouseY.set(event.yPixel);
+  }
+}
