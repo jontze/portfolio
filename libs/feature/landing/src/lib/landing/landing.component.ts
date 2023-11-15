65116df4940a91416441464cc8b408d1be7fa06d
Jontze <42588836+jontze@users.noreply.github.com>
2023-11-15 22:35:28 +0100

    chore(libs): Generate landing feature lib

---
+++ libs/feature/landing/src/lib/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'portfolio-landing',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.css'],
+})
+export class LandingComponent {}
