dab1693e73f39199e4b8067e3fdda87ec1cc6924
Jontze <42588836+jontze@users.noreply.github.com>
2023-11-20 23:22:49 +0100

    feat(Landing): Animate icon with fade-in on initial render

--- libs/feature/landing/src/lib/landing/landing.component.ts
+++ libs/feature/landing/src/lib/landing/landing.component.ts
@@ -1,10 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { fadeIn } from '@jontze/ui/animations';
 import { Icon } from '@jontze/ui/icon';
 
 @Component({
   selector: 'portfolio-landing',
   templateUrl: './landing.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [fadeIn(20, 3000)],
 })
 export class LandingComponent {
   chevronDownIcon = Icon.ChevronDoubleDown;
