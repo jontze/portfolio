6cd147e23662a7d1e1e74cf76a8e9c1d7b560827
jontze <42588836+jontze@users.noreply.github.com>
2024-06-08 13:04:29 +0200

    feat(angular): Get rid of zone.js

--- apps/portfolio/src/main.ts
+++ apps/portfolio/src/main.ts
@@ -3,10 +3,12 @@ import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { provideExperimentalZonelessChangeDetection } from '@angular/core';
 import { appRoutes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
   providers: [
+    provideExperimentalZonelessChangeDetection(),
     provideAnimations(),
     provideHttpClient(),
     provideRouter(appRoutes),
